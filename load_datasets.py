d7d3723c13159172be879d8d0b95375c01f08f5b
ViswaSri7 <pasupuletiviswasri@gmail.com>
2024-10-03 11:31:09 -0500

    code clean

--- load_datasets.py
+++ load_datasets.py
@@ -4,7 +4,7 @@ import pandas as pd
 dataset1 = pd.read_csv('feed.csv')  # Replace with your actual file name
 dataset2 = pd.read_csv('phishing.csv')  # Replace with your actual file name
 dataset3 = pd.read_csv('PhishingData.csv')  # Replace with your actual file name
-dataset4 = pd.read_csv('Training Dataset.csv')  # Replace with your actual file name
+#dataset4 = pd.read_csv('Training Dataset.csv')  # Replace with your actual file name
 
 # Combine the datasets into one DataFrame
 combined_dataset = pd.concat([dataset1, dataset2, dataset3], ignore_index=True)
